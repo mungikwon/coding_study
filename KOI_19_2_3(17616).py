1dfba257672c61f902e3ec11226b5ec7c315006d
mungikwon <mgkwon0502@gmail.com>
2020-06-22 12:29:54 +0900

    upload

--- KOI_19_2_3(17616).py
+++ KOI_19_2_3(17616).py
@@ -4,6 +4,47 @@ Created on Mon May 18 14:43:39 2020
 
 @author: USER
 """
+
+from collections import deque
+
+
+def bfs(X,n_list):
+    result=0
+    visited=[0]*(N+1)
+    q=deque()
+    q.append(X)
+    visited[X]=1
+    while q:
+        v=q.popleft()
+        for i in n_list[v]:
+            if visited[i]==0:
+                q.append(i)
+                visited[i]==1
+                result=result+1
+    return result
+
+
+
+
+
+N,M,X = map(int,input().split())
+lower=[[] for _ in range(N+1)]
+higher=[[] for _ in range(N+1)]
+
+for i in range(M):
+    A,B=map(int,input().split())
+    lower[B].append(A)
+    higher[A].append(B)
+
+print(1+bfs(X,lower),Nbfs-(X,higher))
+   
+
+
+
+# different idea
+
+""" 
+
 from collections import deque
 
 N,M,X = map(int,input().split())
@@ -38,4 +79,7 @@ for _ in range(M):
     higher[A].append(B)
     
 print(1+bfs(X,lower),N-bfs(X,higher))
-    
+     """
+
+
+
