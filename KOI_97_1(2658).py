a0e0fd225a005d981ab9df8f9301d7490f8c0ac9
mungikwon <mgkwon0502@gmail.com>
2020-06-30 16:18:02 +0900

    2020.06.30

---
+++ KOI_97_1(2658).py
@@ -0,0 +1,38 @@
+room=[[] for _ in range(10)]
+
+nx=[0,-1,0,1]
+ny=[-1,0,1,0]
+lo=[]
+
+def check(y,x):
+    result=0
+    for k in range(4):
+        dx=x+nx[k]
+        dy=y+ny[k]
+        if room[dy][dx]==0:
+            result+=1
+
+    if result==3:
+         lo.append([y+1,x+1])       
+
+
+
+
+for i in range(10):
+    room[i]=list(map(int,input()))
+
+
+for i in range(10):
+    for j in range(10):
+        if room[i][j]==1:
+            check(i,j)
+
+
+if len(lo)==2:
+    lo.append([lo[0][0],lo[1][1]])
+
+lo.sort()
+if len(lo)==3:
+    print(lo)
+else:
+    print("0")
