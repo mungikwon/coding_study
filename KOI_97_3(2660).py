9d561339aa5189fa529bc03a09df1a6e39e222ea
mungikwon <mgkwon0502@gmail.com>
2020-07-07 16:56:24 +0900

    2020.07.07

--- KOI_97_3(2660).py
+++ KOI_97_3(2660).py
@@ -7,23 +7,11 @@ people=[[] for _ in range(N+1)]
 
 
 
-def paly(a):
-    check=[0]*(N+1)
-    count=0
-    q=collections.deque()
-    while(check.count==N):
-        for i in people[a]:
-            q.append(i)
-            check[i]=1
+# def paly(a):
+#     check=[0]*(N+1)
+#     count=0
+#     q=collections.deque()
 
-    
-        for i in people[a]:
-            q.append(i)
-            check[i]=1
-
-    
-        a=q.popleft()
-        for j in people[a]:
 
         
 
@@ -37,3 +25,4 @@ while(a!=-1):
         people[a].append(b)
         people[b].append(a)
 print(people)
+print(people[2])
